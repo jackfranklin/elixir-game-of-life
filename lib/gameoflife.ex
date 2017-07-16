822858c92742cfbce7105b85766954774e4135a8
Jack Franklin <jack@jackfranklin.net>
2017-07-16 19:07:32 +0100

    Implementation

--- lib/gameoflife.ex
+++ lib/gameoflife.ex
@@ -7,8 +7,21 @@ defmodule GameOfLife do
     %{board: cells}
   end
 
+  def tick(game) do
+    cells_that_live = Enum.filter(game.board, fn cell -> cell_should_die?(game, cell) == false end)
+    new_cells = get_new_cells(game)
+    %{board: Enum.concat(cells_that_live, new_cells)}
+  end
+
   def neighbours?(%{x: x1, y: y1} = cell1, %{x: x2, y: y2} = cell2) do
     abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1 && !cells_equal?(cell1, cell2)
+  end
+
+  defp get_new_cells(game) do
+    game.board
+    |> Enum.flat_map(fn cell -> potential_neighbours_for_cell(cell) end)
+    |> Enum.uniq
+    |> Enum.filter(fn cell -> cell_should_come_alive?(game, cell) end)
   end
 
   def potential_neighbours_for_cell(cell) do
@@ -18,10 +31,30 @@ defmodule GameOfLife do
   end
 
   def neighbours_of_cell(%{board: board}, cell) do
-    # board |> Enum.filter(fn board_cell
-    []
+    potential_neighbours = potential_neighbours_for_cell(cell)
+    board
+    |> Enum.filter(fn board_cell -> cells_equal?(cell, board_cell) == false end)
+    |> Enum.filter(fn board_cell -> Enum.member?(potential_neighbours, board_cell) end)
   end
 
+
+  def cell_count(game) do
+    Enum.count(game.board)
+  end
+
+  def cell_should_die?(game, cell) do
+    neighbours = neighbours_of_cell(game, cell)
+    Enum.count(neighbours) < 2 || Enum.count(neighbours) > 3
+  end
+
+  def cell_should_come_alive?(game, cell) do
+    neighbours = neighbours_of_cell(game, cell)
+    Enum.count(neighbours) == 3 && cell_is_alive?(game, cell) == false
+  end
+
+  def cell_is_alive?(game, cell) do
+    Enum.member?(game.board, cell)
+  end
 
   defp cells_equal?(cell1, cell2) do
     cell1.x == cell2.x && cell1.y == cell2.y
@@ -41,5 +74,4 @@ defmodule GameOfLife do
   defp make_coordinate_with_x(x) do
     fn y -> %{x: x, y: y} end
   end
-
 end
--- test/gameoflife_test.exs
+++ test/gameoflife_test.exs
@@ -2,14 +2,18 @@ defmodule GameoflifeTest do
   use ExUnit.Case
   doctest GameOfLife
 
+  def game_has_cells(game, cells) do
+    Enum.all?(cells, fn cell -> GameOfLife.cell_is_alive?(game, cell) end)
+      && Enum.count(game.board) == Enum.count(cells)
+  end
+
   test "a board is empty by default" do
-    %{ board: board } = GameOfLife.new_game
-    assert (length board) == 0
+    assert GameOfLife.cell_count(GameOfLife.new_game) == 0
   end
 
   test "a board can be created with cells" do
-    %{ board: board } = GameOfLife.new_game ([%{x: 1, y: 0}])
-    assert (length board) == 1
+    game = GameOfLife.new_game([%{x: 1, y: 0}])
+    assert GameOfLife.cell_count(game) == 1
   end
 
   test "it can tell if two cells are next to each other" do
@@ -52,6 +56,84 @@ defmodule GameoflifeTest do
     assert GameOfLife.neighbours_of_cell(game, cell) == [
       %{x: 1, y: 0},
     ]
+  end
 
+  test "a cell should die if it has <2 neighbours" do
+    cell_that_should_die = %{x: 0, y: 0}
+    game = GameOfLife.new_game([
+      cell_that_should_die,
+      %{x: 1, y: 0},
+    ])
+
+    assert GameOfLife.cell_should_die?(game, cell_that_should_die)
+  end
+
+  test "a cell should die if it has >3 neighbours" do
+    cell_that_should_die = %{x: 0, y: 0}
+    game = GameOfLife.new_game([
+      cell_that_should_die,
+      %{x: 1, y: 0},
+      %{x: 1, y: 1},
+      %{x: 0, y: 1},
+      %{x: 0, y: -1},
+    ])
+
+    assert GameOfLife.cell_should_die?(game, cell_that_should_die)
+  end
+
+  test "a cell should come alive if it has 3 neighbours" do
+    cell_that_should_come_alive = %{x: 0, y: 0}
+    game = GameOfLife.new_game([
+      %{x: 1, y: 0},
+      %{x: 1, y: 1},
+      %{x: 0, y: 1},
+    ])
+
+    assert GameOfLife.cell_should_come_alive?(game, cell_that_should_come_alive)
+  end
+
+  test "a cell should not come alive if it has >3 neighbours" do
+    cell_that_should_come_alive = %{x: 0, y: 0}
+    game = GameOfLife.new_game([
+      %{x: 1, y: 0},
+      %{x: 1, y: 1},
+      %{x: 0, y: 1},
+      %{x: 0, y: -1},
+    ])
+
+    refute GameOfLife.cell_should_come_alive?(game, cell_that_should_come_alive)
+  end
+
+  test "an already existing cell should not come alive" do
+    cell_that_is_alive = %{x: 0, y: 0}
+    game = GameOfLife.new_game([
+      cell_that_is_alive,
+      %{x: 1, y: 0},
+      %{x: 1, y: 1},
+      %{x: 0, y: 1},
+    ])
+
+    refute GameOfLife.cell_should_come_alive?(game, cell_that_is_alive)
+  end
+
+  test "ticking with the blinker" do
+    original_cells = [
+      %{x: -1, y: 0},
+      %{x: 0, y: 0},
+      %{x: 1, y: 0},
+    ]
+
+    game = GameOfLife.new_game(original_cells)
+
+    new_game = GameOfLife.tick(game)
+
+    assert new_game.board == [
+      %{x: 0, y: 0},
+      %{x: 0, y: -1},
+      %{x: 0, y: 1},
+    ]
+
+    final_game = GameOfLife.tick(new_game)
+    assert game_has_cells(final_game, original_cells)
   end
 end
